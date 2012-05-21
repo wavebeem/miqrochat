30904fc8899aa2a37eb2cddf758268081b1562fc
Brian Mock <mock.brian@gmail.com>
2012-05-21 16:54:29 -0700

    Somewhat cool UI now.

--- miqrochat.cpp
+++ miqrochat.cpp
@@ -1,43 +1,58 @@
+/*
+ * This file is part of miqrochat.
+ */
 #include <QApplication>
 #include <QPushButton>
 #include <QToolButton>
+#include <QPlainTextEdit>
 #include <QTreeView>
 #include <QSplitter>
 #include <QLabel>
 #include <QMainWindow>
 #include <QStatusBar>
-#include <QHBoxLayout>
+#include <QVBoxLayout>
 #include <QSpacerItem>
 
 int main(int argc, char **argv) {
+    const int padding = 6;
+    const int none = 0;
+
     QApplication app(argc, argv);
-    QMainWindow window;
+    QMainWindow *window = new QMainWindow;
 
-    QSplitter splitter;
-    QTreeView   friendView;
-    QPushButton b2("Chat");
-    QStatusBar *status = window.statusBar();
-    //status->showMessage("Status");
-    splitter.addWidget(&friendView);
-    splitter.addWidget(&b2);
+    QStatusBar      *status      = window->statusBar();
+    QSplitter       *splitter    = new QSplitter;
+    QTreeView       *friendView  = new QTreeView;
+    QPlainTextEdit  *messageEdit = new QPlainTextEdit;
+    QPlainTextEdit  *chatView    = new QPlainTextEdit;
+    QVBoxLayout     *layout      = new QVBoxLayout;
+    QWidget         *chatArea    = new QWidget;
 
-    const int padding = 3;
-    splitter.setContentsMargins(padding, padding, padding, padding);
+    layout->addWidget(messageEdit);
+    layout->addWidget(chatView);
+    chatArea->setLayout(layout);
+    splitter->addWidget(friendView);
+    splitter->addWidget(chatArea);
 
-    QToolButton settingsButton;
-    settingsButton.setArrowType(Qt::DownArrow);
-    settingsButton.setAutoRaise(true);
-    QLabel statusLabel("Online");
-    QLabel friendLabel("Typing");
-    //status->addPermanentWidget(&settingsButton);
-    status->addWidget(&settingsButton);
-    status->addWidget(&statusLabel);
-    status->addPermanentWidget(&friendLabel);
+    layout->setSpacing(padding);
 
-    window.setCentralWidget(&splitter);
-    window.resize(300, 300);
+    splitter    ->setContentsMargins(padding, padding, padding, padding);
+    layout      ->setContentsMargins(none, none, none, none);
 
-    window.setWindowTitle("Title");
-    window.show();
+    QToolButton *settingsButton = new QToolButton;
+    settingsButton->setArrowType(Qt::DownArrow);
+    settingsButton->setAutoRaise(true);
+    QLabel *statusLabel = new QLabel("Online");
+    QLabel *friendLabel = new QLabel("Typing");
+    status->addWidget(settingsButton);
+    status->addWidget(statusLabel);
+    status->addPermanentWidget(friendLabel);
+
+    //window->setCentralWidget(chatArea);
+    window->setCentralWidget(splitter);
+    window->resize(300, 300);
+
+    window->setWindowTitle("Title");
+    window->show();
     return app.exec();
 }
