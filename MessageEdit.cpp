272d458bbe8c0592ff09d2af307bde82a9c455ec
Brian Mock <mock.brian@gmail.com>
2012-05-22 18:41:39 -0700

    Removed output, added min/max vals.

--- MessageEdit.cpp
+++ MessageEdit.cpp
@@ -10,11 +10,13 @@ MessageEdit::MessageEdit(QWidget *parent)
 
 void MessageEdit::sizeToFit() {
     QSize size = document()->documentLayout()->documentSize().toSize();
-    std::cout << size.width() << ", " << size.height() << std::endl;
     setLineCount(size.height());
 }
 
 void MessageEdit::setLineCount(int rows) {
+    int max = 10;
+    rows = rows < 2?   1:   rows;
+    rows = rows > max? max: rows;
     QFontMetrics m(font());
     int pad = 12;
     int s = m.lineSpacing();
